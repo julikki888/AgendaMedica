1778da7eded85556bfe89db585c180822d913a91
1DAW <1DAW@DESKTOP-84KDH6P>
2026-05-13 14:57:25 +0200

    asd

---
+++ biblioteca/src/modelo/CitaMedica.java
@@ -0,0 +1,15 @@
+package modelo;
+
+import java.util.Calendar;
+import java.util.LinkedList;
+
+public class CitaMedica {
+
+	private int[] prioridad;	//de 0 a 3 de tamaño
+	private String numSeguridad, nombrePaciente, especialista;
+	private LinkedList<String> listaMotivos;
+	private Calendar fecha;
+	
+	
+	
+}
